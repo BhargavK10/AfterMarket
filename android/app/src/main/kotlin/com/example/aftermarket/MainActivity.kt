3198c932279e85e4b6c91318a2b601b42cda6116
BhargavK <bhargavkandalkar2210@gmail.com>
2025-07-16 10:42:34 +0530

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/aftermarket/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aftermarket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/titlebar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+PreferredSizeWidget titleBar(String title){
+  return AppBar(
+    backgroundColor: Color(0xFF2A2D34),
+    title: Center(
+      child: Image.asset(
+        'assets/images/FullLogoWhite.png',
+        height: 200,
+      ),
+    )
+  );
+}
